2a042f2de725c195662254b20ba3c90d77202ca2
Vladiszlav Pauk <paukv@oregonstate.edu>
2023-11-05 04:34:18 -0800

    Refactored viewer app

--- code/viewer/src/app.py
+++ code/viewer/src/app.py
@@ -1,102 +1,15 @@
-import os
-import pickle
 import sys
-from datetime import datetime
-
-from flask import Flask, render_template, request, Markup, jsonify
-from ydata_profiling import ProfileReport
-
 sys.path.append('/Users/studio/Work/Projects/Education/code/')
-from scraper.src.utils import get_latest_version
+
+from flask import Flask
+from views import index, search_urls, about
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-
-def read_latest_data(url, path_to_dataset='/Users/studio/Work/Projects/Education/code/'):
-    latest_version = get_latest_version()
-    latest_data_path = f'{path_to_dataset}dataset/student_data_v{latest_version}.pkl'
-
-    if os.path.exists(latest_data_path):
-        with open(latest_data_path, "rb") as f:
-            df = pickle.load(f)
-
-        # Collect all unique snapshots for all students with the matching URL
-        all_snapshots = set()
-        for _, student_data in df.iterrows():
-            if student_data['URL'] == url:
-                for snapshot in student_data['Snapshots']:
-                    all_snapshots.add(snapshot)
-
-        unique_snapshots = sorted(all_snapshots, reverse=True)  # Sort if needed
-
-        # Format the snapshots for the template
-        snapshots_formatted = []
-        for snap in unique_snapshots:
-            if '/web/' in snap:
-                date_str = snap.split('/web/')[1].split('/')[0][:-6]  # Remove HMS
-                date_formatted = datetime.strptime(date_str, "%Y%m%d").strftime("%m/%d/%Y")
-            else:
-                date_formatted = datetime.now().strftime("%m/%d/%Y")
-            snapshots_formatted.append({'url': snap, 'date': date_formatted})
-
-        return df[df['URL'] == url], snapshots_formatted
-    else:
-        return None, None
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        url = request.form.get("url")
-        try:
-            result, snapshots = read_latest_data(url)
-            if result is not None:
-                result.drop(['Email', 'URL'], axis=1, inplace=True)
-                # result['Snapshots'] = result['Snapshots'].apply(lambda x: len(x))
-                result['Start_Date'] = result['Start_Date'].astype(str)
-                result['End_Date'] = result['End_Date'].astype(str)
-                profile = ProfileReport(result, title="Profiling Report")
-                result_html = profile.to_html()
-                result_safe_html = Markup(result_html)
-                snapshot_dates = snapshots
-
-                # snapshots = result['Snapshots'].iloc[0]
-                # for snap in snapshots:
-                #     if '/web/' in snap:
-                #         date_str = snap.split('/web/')[1].split('/')[0][:-6]  # Remove HMS
-                #         date_formatted = datetime.strptime(date_str, "%Y%m%d").strftime("%m/%d/%Y")
-                #     else:
-                #         date_formatted = datetime.now().strftime("%m/%d/%Y")
-                #     snapshot_dates.append({'url': snap, 'date': date_formatted})
-
-                return render_template("index.html", result=result_safe_html, snapshot_dates=snapshot_dates, error=None)
-            else:
-                return render_template("index.html", result=None, error="No matching data found.")
-        except Exception:
-            return render_template("index.html", result=None, error=f"Please enter a valid URL.")
-    return render_template("index.html", result=None, snapshot_dates=[], error=None)
-
-
-@app.route("/search-urls")
-def search_urls():
-    query = request.args.get('query', '').lower()
-    latest_version = get_latest_version()
-    latest_data_path = f'/Users/studio/Work/Projects/Education/code/dataset/student_data_v{latest_version}.pkl'
-
-    if os.path.exists(latest_data_path):
-        with open(latest_data_path, "rb") as f:
-            df = pickle.load(f)
-        # Ensure we're searching within a string, and drop duplicates
-        matching_urls = df[df['URL'].str.lower().str.contains(query)]['URL'].drop_duplicates().tolist()
-        return jsonify(matching_urls)
-    else:
-        return jsonify([])  # Return an empty list if no matches
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+app.add_url_rule('/about', view_func=about, methods=["GET"])
+app.add_url_rule('/', view_func=index, methods=["GET", "POST"])
+app.add_url_rule('/search-urls', view_func=search_urls, methods=["GET"])
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
