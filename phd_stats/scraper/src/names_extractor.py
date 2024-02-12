ff036498984b9699b5b2750ca0e156bd190889a4
Vladiszlav Pauk <paukv@oregonstate.edu>
2024-02-12 05:01:38 -0800

    feature: add new name extractors

--- phd_stats/scraper/src/names_extractor.py
+++ phd_stats/scraper/src/names_extractor.py
@@ -26,6 +26,26 @@ def extract_names(soup: BeautifulSoup, url: str) -> Tuple[List[str], str, str]:
         return extract_names_wisc(soup)
     elif 'vanderbilt.edu' in url:
         return extract_names_vanderbilt(soup)
+    elif 'ucdavis.edu' in url:
+        return extract_names_ucdavis(soup)
+    elif 'ucsb.edu' in url:
+        return extract_names_ucsb(soup)
+    elif 'ucsc.edu' in url:
+        return extract_names_ucsc(soup)
+    elif 'uic.edu' in url:
+        return extract_names_uic(soup)
+    elif 'stonybrook.edu' in url:
+        return extract_names_stonybrook(soup)
+    elif 'nyu.edu' in url:
+        return extract_names_nyu(soup)
+    elif 'princeton.edu' in url:
+        return extract_names_princeton(soup)
+    elif 'tamu.edu' in url:
+        return extract_names_tamu(soup)
+    elif 'osu.edu' in url:
+        return extract_names_osu(soup)
+    elif 'jhu.edu' in url:
+        return extract_names_jhu(soup)
     else:
         return []
 
@@ -132,3 +152,152 @@ def extract_names_vanderbilt(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
         if strong_tag and strong_tag.text.strip():
             names.append(strong_tag.text.strip())
     return names, 'Vanderbilt University', 'Philosophy'
+
+
+def extract_names_ucdavis(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all h3 tags with the 'vm-teaser__title' class
+    h3_tags = soup.find_all('h3', class_='vm-teaser__title')
+    for h3 in h3_tags:
+        span_tag = h3.find('span', class_='field field--name-title field--type-string field--label-hidden')
+        if span_tag and span_tag.text.strip():
+            names.append(span_tag.text.strip())
+
+    return names, 'UC Davis', 'Philosophy'
+
+
+def extract_names_ucsb(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all span tags with the 'field-content' class
+    span_tags = soup.find_all('span', class_='field-content')
+    for span in span_tags:
+        a_tag = span.find('a')
+        if a_tag and a_tag.text.strip():
+            names.append(a_tag.text.strip())
+
+    return names, 'UC Santa Barbara', 'Philosophy'
+
+
+def extract_names_ucsc(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all span tags with the 'p-name' class
+    span_tags = soup.find_all('span', class_='p-name')
+    for span in span_tags:
+        if span and span.text.strip():
+            names.append(span.text.strip())
+
+    # Additional scenario: Find all h3 tags within a tags, nested inside td tags
+    td_tags = soup.find_all('td', colspan="2")
+    for td in td_tags:
+        h3_tag = td.find('h3')
+        if h3_tag and h3_tag.text.strip():
+            names.append(h3_tag.text.strip())
+
+    return names, 'UC Santa Cruz', 'Philosophy'
+
+
+def extract_names_uic(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all h3 tags with the '_title' class
+    h3_tags = soup.find_all('h3', class_='_title')
+    for h3 in h3_tags:
+        span_tag = h3.find('span', class_='_name')
+        if span_tag:
+            a_tag = span_tag.find('a')
+            if a_tag and a_tag.text.strip():
+                names.append(a_tag.text.strip())
+
+    names = [name.replace('\n', '').strip() for name in names]
+    names = [name.split(',')[1].strip() + ' ' + name.split(',')[0].strip() for name in names]
+
+    return names, 'University of Illinois Chicago', 'Philosophy'
+
+
+def extract_names_stonybrook(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all h3 tags
+    h3_tags = soup.find_all('h3')
+    for h3 in h3_tags:
+        if h3 and h3.text.strip():
+            names.append(h3.text.strip())
+
+    return names, 'Stony Brook University', 'Philosophy'
+
+
+def extract_names_nyu(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all h5 tags and extract names from nested a tags
+    h5_tags = soup.find_all('h5')
+    for h5 in h5_tags:
+        a_tag = h5.find('a')
+        if a_tag and a_tag.text.strip():
+            names.append(a_tag.text.strip())
+
+    return names, 'New York University', 'Philosophy'
+
+
+def extract_names_princeton(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Define the phrases to filter out
+    filter_phrases = ["Princeton University", "Department of Philosophy"]
+
+    # Find all img tags and extract names from the alt attribute
+    img_tags = soup.find_all('img')
+    for img in img_tags:
+        alt_text = img.get('alt', '').strip()
+        # Check and exclude the filter phrases
+        if alt_text and not any(filter_phrase in alt_text for filter_phrase in filter_phrases):
+            names.append(alt_text)
+
+    return names, 'Princeton University', 'Philosophy'
+
+
+def extract_names_tamu(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all span tags with the 'name' class and extract names from nested a tags
+    span_tags = soup.find_all('span', class_='name')
+    for span in span_tags:
+        a_tag = span.find('a')
+        if a_tag and a_tag.text.strip():
+            names.append(a_tag.text.strip())
+
+    print(names)
+    return names, 'Texas A&M University', 'Philosophy'
+
+
+def extract_names_osu(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Find all span tags with the 'field-content' class and extract names from the title attribute of nested a tags
+    span_tags = soup.find_all('span', class_='field-content')
+    for span in span_tags:
+        a_tag = span.find('a')
+        if a_tag and a_tag.get('title', '').strip():
+            names.append(a_tag['title'].strip())
+
+    return names, 'Ohio State University', 'Philosophy'
+
+
+def extract_names_jhu(soup: BeautifulSoup) -> Tuple[List[str], str, str]:
+    names = []
+
+    # Define the phrases to filter out
+    filter_phrases = ["Logo: Johns Hopkins University", "Home"]
+
+    # Find all img tags and extract names from the alt attribute
+    img_tags = soup.find_all('img')
+    for img in img_tags:
+        alt_text = img.get('alt', '').strip()
+        # Check and exclude the filter phrases
+        if alt_text and not any(filter_phrase in alt_text for filter_phrase in filter_phrases):
+            names.append(alt_text)
+
+    return names, 'Johns Hopkins University', 'Philosophy'
