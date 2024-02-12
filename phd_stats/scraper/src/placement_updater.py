052f6c828982b5954b83da72ec5175d2b101e63a
Vladiszlav Pauk <paukv@oregonstate.edu>
2024-02-12 05:05:33 -0800

    feature: update name matching in placement search

--- phd_stats/scraper/src/placement_updater.py
+++ phd_stats/scraper/src/placement_updater.py
@@ -39,7 +39,31 @@ def update_placement_from_webpage(database_df: pd.DataFrame, url: str) -> pd.Dat
     html_content = soup_object.get_text()
 
     webpage_names = set(re.findall(r'\b[A-Z][a-z]+ [A-Z][a-z]+\b', html_content))
-    database_df['Placement'] = database_df['Name'].isin(webpage_names)
+
+    def preprocess_name(name):
+        # Split the name into parts and sort them alphabetically
+        return sorted(name.replace(',', '').split())
+
+    def check_two_word_match(name_parts, webpage_name_parts_list):
+        # Check if at least two words match between the name parts and any of the webpage name parts
+        for webpage_name_parts in webpage_name_parts_list:
+            match_count = sum(part in webpage_name_parts for part in name_parts)
+            if match_count >= 2:
+                return True
+        return False
+
+    # Preprocess all names in the database
+    database_df['ProcessedName'] = database_df['Name'].apply(preprocess_name)
+
+    # Preprocess all names from the webpage
+    processed_webpage_names = [preprocess_name(name) for name in webpage_names]
+
+    # Check if at least two words in the processed name from the database match with any of the processed webpage names
+    database_df['Placement'] = database_df['ProcessedName'].apply(
+        lambda x: check_two_word_match(x, processed_webpage_names))
+
+    database_df.drop('ProcessedName', axis=1, inplace=True)
+
     matching_placements = database_df['Placement'].sum()
     logging.info(f"Found {matching_placements} placements in {url}")
 
